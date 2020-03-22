0e9b87d74b81d96bd7d060a092bc861f63e93aad
Ashwin Haridas <haridas.ashwin@gmail.com>
2020-03-22 01:06:32 -0400

    prints all files and subdirectories

--- README.md
+++
@@ -1,2 +0,0 @@
-# Asst1
-CS 214: Systems Programming Assignment 1
---
+++ fileCompressor.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
+
+int isDirectory(const char* path)
+{
+    struct stat statbuf;
+    if(stat(path, &statbuf) != 0)
+        return 0;
+    
+    return S_ISDIR(statbuf.st_mode);
+}
+
+void printAllFiles(char* basePath)
+{
+    DIR* dir = opendir(basePath);
+    readdir(dir);
+    readdir(dir);
+    char path[10000];
+
+    struct dirent* entry;
+
+    while((entry = readdir(dir)) != NULL)
+    {
+        strcpy(path, basePath);
+        strcat(path, "/");
+        strcat(path, entry->d_name);
+
+        if(isDirectory(path))
+            printAllFiles(path);
+        else
+        {
+            printf("%s\n", entry->d_name);
+        }
+    }
+    
+    closedir(dir);
+}
+
+int main(int argc, char* argv[])
+{
+    printAllFiles("./");
+    return 0;
+}

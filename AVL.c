b1f5af3502bdd5089f9d048926a9b2c8f4d0de65
Ritin R. Nair <rrn32@ilab1.cs.rutgers.edu>
2020-03-29 18:42:22 -0400

    added to AVL.c

--- AVL.c
+++ AVL.c
@@ -1,1 +1,46 @@
 #include "structs.h"
+
+node* add(node* root, node* strNode){
+    if( root == NULL){ // tree is empty
+        return strNode;
+    }
+
+    if(strcmp(root->token, strNode->token) == 0){ // already exists in the tree so increment frequency
+        root->freq++;
+        return root;
+    }
+
+    if(strcmp(root->token, strNode->token) > 0){ // traverse through right subtree
+        root->right = add(root->right, strNode);
+    }
+
+    if(strcmp(root->token, strNode->token) < 0){ // traverse through left subtree
+        root->left = add(root->left, strNode);
+    }
+
+    return root; // to traverse back up the tree recursively to the original root and exit the function
+}
+
+void heights(node* root){
+    if (root == NULL){ 
+        return;
+    }
+
+    heights(root->left); // recursively assigns heights for left subtree
+    heights(root->right); // recursively assigns height for right subtree
+
+    root->height = -1; // height of empty node is -1
+
+    if(root->left != NULL){ // node has a left child so set its height temporarily equal to this
+        root->height = root->left->height;
+    }
+
+    if(root->right != NULL){
+        if(root->right->height > root->height){ // if the right child has a height greater than the current height 
+            root->height = root->right->height; //reassign height;
+        }
+    }
+
+    root->height++; // height of a node is its largest child's height plus one (zero if a node has no children)
+}
+
--- structs.h
+++ structs.h
@@ -1,12 +1,17 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 typedef struct node
 {
     char* token;
     struct node* left;
     struct node* right;
     int freq;
+    int height;
 } node;
 
 #endif
